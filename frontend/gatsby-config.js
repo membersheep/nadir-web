798242f0ec0d51417e203dcb665b06fe90e74285
Alessandro Maroso <>
2020-05-29 00:53:24 +0200

    Update to use heroku

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: process.env.API_URL || "http://localhost:1337",
+        apiURL: process.env.API_URL || "https://nadir-backend.herokuapp.com",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "section",
