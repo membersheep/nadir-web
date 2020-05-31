a7a9d88c276db1ad6f8bbc538afaa660e49b71e8
Alessandro Maroso <alessandro.maroso@gmail.com>
2020-05-31 16:34:17 +0200

    Make page cover image configurable

--- src/templates/page.js
+++ src/templates/page.js
@@ -9,19 +9,26 @@ export const query = graphql`
       strapiId
       title
       text
+      cover {
+        publicURL
+      }
     }
   }
 `
 
 const Page = ({ data }) => {
   const page = data.strapiPage
+  var coverImage = "/bg.jpg"
+  if (page.cover != null) {
+    coverImage = page.cover.publicURL
+  }
   return (
     <Layout>
       <div class="uk-cover-container uk-light uk-flex uk-flex-middle top-wrap-height-page">
         <div class="uk-container uk-flex-auto top-container uk-position-bottom-left uk-margin-medium-top">
           <h1 class="uk-margin-remove-top">{page.title}</h1>
         </div>
-        <img src="/bg.jpg" data-sizes="20vw" alt="" data-uk-cover data-uk-img data-uk-parallax="opacity: 1,0.1; easing:0"/>
+        <img src={coverImage} data-sizes="20vw" alt="" data-uk-cover data-uk-img data-uk-parallax="opacity: 1,0.1; easing:0"/>
       </div> 
       <div className="uk-container uk-container-small uk-margin uk-text">
         <ReactMarkdown source={page.text} />
