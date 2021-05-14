cc5d83038caa31da0448330023991567d506cbfb
Alessandro Maroso <alessandro.maroso@rawfish.com>
2021-05-14 13:15:54 +0200

    Fix 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,6 +6,7 @@ import Seo from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
+    <div id="nav-spacer"></div>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
